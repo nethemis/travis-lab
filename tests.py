7900fb6e364911c597bd4503c8c0339d35f6b62a
nethemis <neofytos.themistokleous@cern.ch>
2018-10-25 16:52:06 +0100

    this

--- tests.py
+++ tests.py
@@ -1,8 +1,16 @@
 import importlib
+import unittest
 
 importlib.import_module("hello")
 
-class TestCase():
-    def setUp(self):
-        return self.assertEqual(hello.hi(), "Hello World!!")
+class TestCase(unittest.TestCase):
+    def test(self):
+        #result =  self.assertEqual(hello.hello_function(), "Hello World!!")
+        result =  self.assertEqual(1, 1,"This is wrong")
+        print(result)
+        return result
 
+
+
+MyTest = TestCase()
+MyTest.test()
